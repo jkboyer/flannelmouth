be12353c19e68cc85deef2021b8cfe9f21d10a7c
ltennant13 <ltennant@usgs.gov>
2020-12-18 13:07:46 -0700

    filling in missing RMs and removing records with NAs

--- FMS_Fix_reach_length.R
+++ FMS_Fix_reach_length.R
@@ -1,5 +1,5 @@
 # Testing script before I fuck up GitHub code
-# CH_reach = need to:
+# # CH_reach = need to:
 # 1.fill in missing Rms for fish/trips that need it
 # 2.remove fish that have no RMs associated with it
 #   Basically no "NAs" should exist
@@ -14,69 +14,95 @@
 library(tidyverse)
 
 #load station.csv files that have station information needed
-file.names <- list.files(
-  path ="C:/Users/ltennant/Desktop/FMS_mark_recap/QAQC")
-FMS <- read.csv("C:/Users/ltennant/Desktop/FMS_mark_recap/flannelmouth/data/all_PIT_tagged_flannelmouth.csv") #all FMS
-MRS <- read.csv("C:/Users/ltennant/Desktop/FMS_mark_recap/QAQC/BB_Mechanical_removal_stations.csv",
-                   stringsAsFactors = FALSE) #mechanical removal stations
-LFS <- read.csv("C:/Users/ltennant/Desktop/FMS_mark_recap/QAQC/BB_LF_Stations.csv",
-                stringsAsFactors = FALSE) #Lee's Ferry stations
-
-FMS <- read.csv("./data/all_PIT_tagged_flannelmouth.csv") #all FMS
-MRS <- read.csv("./data/BB_Mechanical_removal_stations.csv",
+fms <- read.csv("./data/all_PIT_tagged_flannelmouth.csv") #all FMS ############ Need to update - only has 30K tags, needs 90K!!!!!!!!!!!!!!!!!!!!!!!!!
+mrs <- read.csv("./data/BB_Mechanical_removal_stations.csv",
                 stringsAsFactors = FALSE) #mechanical removal stations
-LFS <- read.csv("./data/BB_LF_Stations.csv",
+lfs <- read.csv("./data/BB_LF_Stations.csv",
                 stringsAsFactors = FALSE) #Lee's Ferry stations
 
-View(FMS)
-View(MRS)
+View(fms)
+View(mrs)
+View(lfs)
 #####################################################################
-#Jan's attempt
-# merge station data based on unpadded station id
+#Correcting for missing reach information
+#merge station data based on unpadded station id
 #then fill in mile if mile is missing
 
 #column names need to match (for column we are joining on)
-colnames(FMS)
+colnames(fms)
 
 #subset to just data missing mile to see missing data
-fms.missing <- FMS %>%
+fms.missing <- fms %>%
   filter(is.na(START_RM) & RIVER_CODE == "COR")
 
 #when joining, subset table to be joined to only necessary columns
-MRS <- MRS %>%
+mrs <- mrs %>% #need to associate mechanical removal station IDs to get RM
   #column names must match for joining column
   transmute(STATION_ID = UNPADDED_STATION_ID,
             #and not match for other columns
             mile = START_RM)
 
-#join data from station table on to fms
-FMS <- FMS %>%
-  left_join(MRS) #left join keeps all FMS columns, only MRS cols that match
+#join data from mechanical removal station table on to fms
+fms <- fms %>%
+  left_join(mrs) #left join keeps all FMS columns, only MRS cols that match
 
 # if else statement to add mile from station only if start_RM is missing
-FMS$START_RM <- ifelse(!is.na(FMS$START_RM), FMS$START_RM, FMS$mile)
+fms$START_RM <- ifelse(!is.na(fms$START_RM), fms$START_RM, fms$mile)
 
+# join data from the Lee's Ferry station table on to fms
+lfs <- lfs %>% #need to associate Lee's Ferry station IDs to get RM
+  transmute(STATION_ID = UNPADDED_STATION_ID,
+            mile2 = DOWN_RM)
 
-# work on correcting reach information first
+fms <- fms %>%
+  left_join(lfs)
 
+# if else statement to add mile from station only if start_RM is missing
+fms$START_RM <- ifelse(!is.na(fms$START_RM), fms$START_RM, fms$mile2)
 
+# remove mile columns from fms
+fms <- fms %>%
+  select(-mile, -mile2)
 
+# what else is missing for RMs and RKMs (inlcudes LCR, HAV, etc.)
+fms.missing <- fms %>%
+  filter(is.na(START_RM) & is.na(START_RKM))
 
-FMS <- FMS %>%
-  mutate(START_RM = case_when(TRIP_ID == "GC20090430" & is.na(START_RM) ~ 108.6,
+# remove all RM = NA for where we couldn't find the RM information for COR (only 27 records) -
+# did it for RKMs for tribs as well (18 records) = 45 records
+fms <- fms %>%
+  filter(!is.na(START_RM) | !is.na(START_RKM))
+
+# double checking all records were removed
+fms.missing <- fms %>%
+  filter(is.na(START_RM) & is.na(START_RKM))
+
+# other weird shit happening with tribs
+trib <- fms %>%
+  filter(RIVER_CODE !="COR" & !is.na(START_RM))
+view(trib)
+
+#LCR only trib with weird RMs; remove records where LCR has START_RM weirdness (n=20)
+
 
 
 
 # ------------------------------------------------------#
-colnames(MRS)[2] <- "MRS_START_RM"
 
-#remove columns not applicable
-MRS <- MRS %>%
-  select (-c(END_RM,DB_LINK_ID,STATION_TYPE,RIVER_CODE,
-             SIDE_CODE,START_RKM,END_RKM,STATION_ID))
 
-FMS <- FMS %>%
-  bind_rows(MRS)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
